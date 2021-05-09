0e706625576388e24027ad0ec2bf7688cb4fecb3
Alberto Sanchez <albertonatanael486@gmail.com>
2021-05-08 21:19:53 -0500

    Completo

---
+++ Controllers/EmpleadoesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ExamenDWBE.Models;
+using ExamenDWBE.Helper;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ExamenDWBE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class EmpleadoesController : Controller
+    {
+        private readonly RFIDContext context;
+
+        public EmpleadoesController(RFIDContext _context)
+        {
+            context = _context;
+        }
+
+        // GET: Empleadoes
+        [HttpGet]
+        public async Task<IEnumerable<Empleado>> Index()
+        {
+            return await context.Empleado.ToListAsync();
+        }
+
+        // GET: Empleadoes/Details/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmpleadobyId(int id)
+        {
+            var empleado = await context.Empleado
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (empleado == null)
+            {
+                return NotFound(ErrorHelper.Response(404,"No existe ese id"));
+            }
+
+            return Ok(empleado);
+        }
+        
+        // POST: api/empleadoes
+        [HttpPost]
+        public async Task<IActionResult> Post([Bind("Nombre,Rfid")] Empleado empleado)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ErrorHelper.GetModelStateErrors(ModelState));            
+            }
+
+            if(await context.Empleado.Where(x => x.Rfid == empleado.Rfid).AnyAsync())
+            {
+                return BadRequest(ErrorHelper.Response(400, "el rfid ya existe."));
+            }
+            context.Add(empleado);
+            await context.SaveChangesAsync();
+            return Created($"/empleadoes/{empleado.Id}", empleado);
+        }
+    }
+}
---
+++ Controllers/IngresosController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ExamenDWBE.Models;
+using ExamenDWBE.Helper;
+using Microsoft.AspNetCore.Authorization;
+using ExamenDWBE.Models.ViewModels;
+
+namespace ExamenDWBE.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class IngresosController : Controller
+    {
+        private readonly RFIDContext context;
+
+        public IngresosController(RFIDContext _context)
+        {
+            context = _context;
+        }
+
+        [HttpGet]
+        // GET: Ingresos
+        public async Task<IEnumerable<Ingresos>> Index()
+        {
+            return await context.Ingresos.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        // GET: Ingresos/Details/5
+        public async Task<IActionResult> GetRegistroById(int id)
+        {
+            var ingreso = await context.Ingresos
+                .FirstOrDefaultAsync(m => m.RegistroId == id);
+            if (ingreso == null)
+            {
+                return NotFound(ErrorHelper.Response(404, "No existe ese id"));
+            }
+
+            return Ok(ingreso);
+        }
+
+        [HttpGet("byEmp/{id}")]
+        // GET: Ingresos/Details/5
+        public async Task<IActionResult> GetRegistroByEmpleadoId(int id)
+        {
+            var empleado = await context.Empleado.FirstOrDefaultAsync(m => m.Id == id);
+            if (empleado == null)
+            {
+                return NotFound(ErrorHelper.Response(404, "No existe ese id de empleado"));
+            }
+
+            var request = await context.Ingresos.Where(x => x.EmpleadoId == empleado.Id).ToListAsync();
+            var ingresos = new List<IngresoVM>();
+
+            foreach(Ingresos item in request){
+                ingresos.Add(new IngresoVM
+                {
+                    RegistroId = item.RegistroId,
+                    Fecha = item.Fecha
+                });
+            }
+
+            return Ok(new IngresoEmpleadoVM
+            {
+                EmpleadoId = id,
+                Nombre = empleado.Nombre,
+                ingresos = ingresos
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([Bind("EmpleadoId")] Ingresos ingreso)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ErrorHelper.GetModelStateErrors(ModelState));
+            }
+            
+            ingreso.Fecha = DateTime.Now;
+            context.Add(ingreso);
+            await context.SaveChangesAsync();
+            return Created($"/ingresos/{ingreso.RegistroId}", ingreso);
+            
+        }
+    }
+}
